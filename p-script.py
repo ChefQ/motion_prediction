559e7a4bc40ec190e7ecd4942ede78ad03ed2cff
Oluwaseun Cardoso <oluwaseun.cardosos@mail.utoronto.ca>
2023-12-19 04:07:41 +0100

    1. I added tfidf features and model prediction in p-script. 2. I also changed how to use p-script. Please refer to READ.Me soon for more details.

--- p-script.py
+++ p-script.py
@@ -24,6 +24,7 @@ from datasets import DatasetDict, concatenate_datasets
 from sentence_transformers import SentenceTransformer
 from joblib import dump, load
 import argparse
+import joblib
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
 
@@ -96,74 +97,100 @@ def get_mean_embedding(embedding):
 
 
 ##Arguments
-if __name__ == "__main__":
+
+
+# model_name = "KNN"
+# feature = 'tfidf'
+# #for model_name in ["RFT", "SGD"]: #["KNN","LinearSVC", "Logistic", "RFT", "SGD"]:
+# data = 'dataset/testset.csv'
+if __name__ == "__main__": #True:
+    model_types = ["KNN","LinearSVC", "Logistic", "RFT", "SGD"]
     parser = argparse.ArgumentParser(description='End to end pipeline from briefs to predictions')
-    parser.add_argument('--model_path', default='models/RFT.pkl')
+    parser.add_argument('--model_name', default='RFT', help=f'There are {len(model_types)} models to choose from: {model_types}')
     parser.add_argument('--data',  default= 'embeddings.csv')
+    parser.add_argument('--feature', default='tfidf', help='There are two features to choose from: sentence_embeddings and tfidf')
 
 
 
     arg = parser.parse_args()
 
-    model_name = re.match(r"models/(.*).pkl", arg.model_path).group(1)   
-    embedding_method = summerizeEmbeddings
-   
-    embedding_method = summerizeEmbeddings
-
-    testset = pd.read_csv(arg.data, index_col=0)
+    testset = pd.read_csv(arg.data, index_col=0)# pd.read_csv(data, index_col=0)#
 
     testset["completion"] = list(map(lambda x : x.strip() ,testset["completion"].to_list()))
-    embedding_method = summerizeEmbeddings
+    
 
     summerize = True
-    if 'embeddings' not in testset.columns:
-        if not summerize:
-            model  = "mistralai/Mistral-7B-v0.1"
-            config = AutoConfig.from_pretrained(model)
-            max_input_size =  config.max_position_embeddings  
-            tokenizer = AutoTokenizer.from_pretrained(model, device=device, )
 
-            tokenizer.pad_token = tokenizer.eos_token
-            model = AutoModel.from_pretrained(model, torch_dtype= torch.bfloat16 ).to(device)  #,device_map = "auto"
-            #parallel_model = torch.nn.DataParallel(model)
+    if arg.feature == 'sentence_embeddings':#if arg.feature == 'sentence_embeddings':
+        if 'embeddings' not in testset.columns:
+            if not summerize:
+                model  = "mistralai/Mistral-7B-v0.1"
+                config = AutoConfig.from_pretrained(model)
+                max_input_size =  config.max_position_embeddings  
+                tokenizer = AutoTokenizer.from_pretrained(model, device=device, )
+
+                tokenizer.pad_token = tokenizer.eos_token
+                model = AutoModel.from_pretrained(model, torch_dtype= torch.bfloat16 ).to(device)  #,device_map = "auto"
+                #parallel_model = torch.nn.DataParallel(model)
+            else:
+                sentence_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2').to(device)
+
+            testset['feature'] = ""
+            testset['feature'] = testset['prompt'].map(summerizeEmbeddings)
+            testset.to_csv('embeddings.csv')
+
+
         else:
-            sentence_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2').to(device)
+            #get model for predictions
+            turn2list = lambda x: ast.literal_eval(x)
+            testset['feature'] = testset['embeddings'].map(turn2list)
 
-        testset['embeddings'] = ""
-        testset['embeddings'] = testset['prompt'].map(embedding_method)
-        testset.to_csv('embeddings.csv')
-    
-    else:
-        #get model for predictions
-        turn2list = lambda x: ast.literal_eval(x)
-        testset['embeddings'] = testset['embeddings'].map(turn2list)
+
+    elif arg.feature == 'tfidf':# elif arg.feature == 'tfidf':
+        print("Loading tfidf pipes")
+        support_pipe = joblib.load('pipes/support-tfidf.joblib')
+        opposition_pipe = joblib.load('pipes/oppose-tfidf.joblib')
+
+        testset['feature'] = ""
+
+        sparse_matrix = support_pipe.transform(testset["prompt"].loc[testset["brief_type"]=="support"]).toarray()
+
+        testset['feature'].loc[testset["brief_type"]=="support"] =   sparse_matrix.tolist()
+
+        sparse_matrix = opposition_pipe.transform(testset["prompt"].loc[testset["brief_type"]=="opposition"]).toarray()
+
+        testset['feature'].loc[testset["brief_type"]=="opposition"] =   sparse_matrix.tolist()
+        
+
+
+    x_support = np.array(testset["feature"].loc[(testset["brief_type"]=="support") & (testset["data_type"]=="test") ].to_list())  #  np.array(testset['file_path'].to_list())
+    x_opposition = np.array(testset["feature"].loc[(testset["brief_type"]=="opposition") & (testset["data_type"]=="test") ].to_list())  #  np.array(testset['file_path'].to_list())
+        
 
     #get model for predictions
-    x_support = np.array(testset["embeddings"].loc[(testset["brief_type"]=="support") & (testset["data_type"]=="test") ].to_list())  #  np.array(testset['file_path'].to_list())
-    x_opposition = np.array(testset["embeddings"].loc[(testset["brief_type"]=="opposition") & (testset["data_type"]=="test") ].to_list())  #  np.array(testset['file_path'].to_list())
 
-    y_support = np.array( testset["embeddings"].loc[(testset["brief_type"]=="support") & (testset["data_type"]=="test") ].to_list())  # np.array(testset['label'].to_list())
-    y_opposition =  np.array(testset["embeddings"].loc[(testset["brief_type"]=="opposition") & (testset["data_type"]=="test") ].to_list())  # np.array(testset['label'].to_list())
+    support_model_path = f'models/{arg.model_name}-support-{arg.feature}.pkl'
+    opposition_model_path = f'models/{arg.model_name}-opposition-{arg.feature}.pkl'
 
-    clf = load(arg.model_path) 
+    clfs = {"sup" : load(support_model_path)  , "opp" : load(opposition_model_path) }
 
-    if hasattr(clf, 'predict_proba') and callable(getattr(clf, 'predict_proba')):
-        scores_support = clf.predict_proba(x_support)
-        scores_opposition = clf.predict_proba(x_opposition)
+    if hasattr(clfs["sup"], 'predict_proba') and callable(getattr(clfs["sup"], 'predict_proba')):
+        scores_support = clfs['sup'].predict_proba(x_support)
+        scores_opposition = clfs['opp'].predict_proba(x_opposition)
     else:
-        scores_support = clf.decision_function(x_support)
-        scores_opposition = clf.decision_function(x_opposition)
+        scores_support = clfs['sup'].decision_function(x_support)
+        scores_opposition = clfs['opp'].decision_function(x_opposition)
 
-    prediction_opposition = clf.predict(x_opposition)
-    prediction_support = clf.predict(x_support)
+    prediction_opposition = clfs['opp'].predict(x_opposition)
+    prediction_support = clfs['sup'].predict(x_support)
 
     testset.rename(columns={"file_name": "brief","completion": "truth"} , inplace = True  )
 
     support = testset.loc[(testset["brief_type"]=="support") & (testset["data_type"]=="test") ].copy()
     opposition = testset.loc[(testset["brief_type"]=="opposition") & (testset["data_type"]=="test") ].copy()
 
-    support.drop(['data_type','prompt' , 'brief_type','file_path','embeddings'], axis=1, inplace=True)
-    opposition.drop(['data_type', 'prompt' , 'brief_type','file_path','embeddings'], axis=1, inplace=True)
+    support.drop(['data_type','prompt' , 'brief_type','file_path','feature'], axis=1, inplace=True)
+    opposition.drop(['data_type', 'prompt' , 'brief_type','file_path','feature'], axis=1, inplace=True)
 
     support['predict'] = ""
     opposition['predict'] = ""
@@ -180,7 +207,5 @@ if __name__ == "__main__":
     support = support[["brief","predict","score","truth"]]
     opposition = opposition[["brief","predict","score","truth"]]
 
-    support.to_csv(f'{model_name}_supppredictions.csv' , index = False)
-    opposition.to_csv(f'{model_name}_oppopredictions.csv', index = False)
-
-
+    support.to_csv(f'{arg.model_name}-{arg.feature}-supppredictions.csv' , index = False)
+    opposition.to_csv(f'{arg.model_name}-{arg.feature}-oppopredictions.csv', index = False)
